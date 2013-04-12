0b4038eb8be07511b7d2fe74cc6b577fcf7c7942
Brian Beloshapka <bbelos@hotmail.com>
2013-04-11 21:40:48 -0400

    YPG1: MTD: Add device specific partition define.
    Change-Id: I5ef513f0d5c251275ad4111cecaf3ec2d6371472

--- drivers/mtd/onenand/samsung.c
+++ drivers/mtd/onenand/samsung.c
@@ -46,6 +46,8 @@ enum soc_type {
 #include "samsung_gsm.h"
 #elif defined(CONFIG_SAMSUNG_FASCINATE)
 #include "samsung_fascinate.h"
+#elif defined(CONFIG_SAMSUNG_YPG1)
+#include "samsung_galaxyp.h"
 #else
 #error Should not be used on aries devices (can brick!).
 #include "samsung.h"
